5923e4d64b4152755db2aa172492302f5582c5e5
Fabrizio Contu <fab.contu@gmail>
2025-07-15 18:11:48 +0200

    Layout creation

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-dvh w-dvw">
+      <Navbar />
+      <main className="flex-grow font-poppins" id="main-content" tabIndex={-1}>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
